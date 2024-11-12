93cf31819da269a16b8bacbbc2b59398984eca57
QUOC\len71 <len71231@gmail.com>
2024-11-12 17:01:09 +0700

    Update mongo

--- config/MongoDB.cjs
+++ config/MongoDB.cjs
@@ -1,7 +1,7 @@
 // config/MongoDB.cjs
 const mongoose = require('mongoose');
 
-const mongoURL = 'mongodb://localhost:27017/book';
+const mongoURL = 'mongodb+srv://len71231:XTcfnHl8Lvf9FA2C@cluster0.dmz6c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoURL, { })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('MongoDB connection error:', error));
